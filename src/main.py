added vertex normal calculation and fixed face encoding. blender now shows the faces correctly

src/main.py
@@ -2,6 +2,8 @@ import os
 import struct
 import sys
 
+import numpy as np
+
 from pyawd.PyAwdLogger import logger
 from pyawd.awd import AWDDocument
 
@@ -33,23 +35,57 @@ from pyawd.awd.types.streams.VertexPositionsStream import VertexPositionsStream
 
 f_offset = 1
 
+def compute_vertex_normals(vertices, faces):
+    vertices = np.array(vertices)
+    faces = np.array(faces)
+    vertex_normals = {i: np.array([0.0, 0.0, 0.0]) for i in range(len(vertices))}
+    face_normals = []
+
+    for face in faces:
+        v0, v1, v2 = [vertices[i] for i in face]  # Get vertex positions
+        normal = np.cross(v1 - v0, v2 - v0)  # Compute face normal
+        normal = normal / np.linalg.norm(normal)  # Normalize it
+        face_normals.append(normal)
+
+        # Accumulate normal for each vertex
+        for i in face:
+            vertex_normals[i] += normal
+
+    # Normalize vertex normals
+    for i in vertex_normals:
+        vertex_normals[i] /= np.linalg.norm(vertex_normals[i])
+
+    return vertex_normals
 
 def build_obj(block):
     ret = ""
     cnt = 0
     global f_offset
     for mesh in block.sub_meshes:
+        faces = []
+        verts = []
+        uv = []
         for geo in mesh.geometry_data_blocks:
             if isinstance(geo, VertexPositionsStream):
-                for vertex in geo.vertices:
-                    ret += f"v {vertex[0]} {vertex[1]} {vertex[2]}\n"
-                    cnt += 1
+                verts = geo.vertices
             elif isinstance(geo, FaceIndexStream):
-                for face in geo.faces:
-                    ret += f"f {face[0] + f_offset}/{face[0] + f_offset} {face[1] + f_offset}/{face[1] + f_offset} {face[2] + f_offset}/{face[2] + f_offset}\n"
+                faces = geo.faces
             elif isinstance(geo, UVCoordinatesStream):
-                for coordinate in geo.coordinates:
-                    ret += f"vt {coordinate[0]} {1.0 - coordinate[1]}\n"
+                uv = geo.coordinates
+        for vertex in verts:
+            ret += f"v {vertex[0]} {vertex[1]} {vertex[2]}\n"
+            cnt += 1
+        
+        # generate vn
+        normals = compute_vertex_normals(vertices=verts, faces=faces)
+        for i, coords in normals.items():
+            ret += f"vn {coords[0]} {coords[1]} {coords[2]}\n"
+
+        for face in faces:
+            ret += f"f {face[0] + f_offset}//{face[0] + f_offset} {face[1] + f_offset}//{face[1] + f_offset} {face[2] + f_offset}//{face[2] + f_offset}\n"
+
+        for coordinate in uv:
+            ret += f"vt {coordinate[0]} {1.0 - coordinate[1]}\n"
     f_offset += cnt
     return ret
 